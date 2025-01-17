52f1d7dec15d8931da6466693a5cb5cba5e7c952
mongsam2 <songjaehyun121476@gmail.com>
2025-01-17 16:34:19 +0900

    [feat] 파일 업로드 경로 분리

--- documents/urls.py
+++ documents/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 from .views import DocumentCreateView
-from django.conf import settings
-from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('', DocumentCreateView.as_view(), name='document-create'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
