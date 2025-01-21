189768f94fc54a6be388d24a7c3931f82dc965c0
mongsam2 <songjaehyun121476@gmail.com>
2025-01-21 16:45:10 +0900

    [feat] ocr 추출 api 테스트

--- aims/views.py
+++ aims/views.py
@@ -8,7 +8,7 @@ from documents.models import Document
 
 
 from django.conf import settings
-import os
+import requests
 
 # Create your views here.
 def get_document_path(document_id):
@@ -16,39 +16,38 @@ def get_document_path(document_id):
         document = Document.objects.get(id=document_id)
     except Document.DoesNotExist:
         raise NotFound('그 아이디는 없는 아이디요')
-    file_path = 'http://127.0.0.1:8000'+document.file_url.url
-    return file_path
+    #file_path = 'http://127.0.0.1:8000'+document.file_url.url
+    return document.file_url.path
+
+api_key = "up_i5WhoWFjRhUXflOznB9DLOMQLcG8n"
 
 class ExtractionView(APIView):
     def post(self, request, document_id):
-        document = Document.objects.get(id=document_id)
         file_path = get_document_path(document_id)
+        url = "https://api.upstage.ai/v1/document-ai/ocr"
+        headers = {"Authorization": f"Bearer {api_key}"}
+
+        # upstage api
+        with open(file_path, "rb") as file:
+            files = {"document": file}
+            response = requests.post(url, headers=headers, files=files)
         
-        return Response({'message': file_path})
+        return Response({'message': response.json()})
     
 class SummarizationView(APIView):
     def post(self, request, document_id):
-        document = Document.objects.get(id=document_id)
         file_path = get_document_path(document_id)
         
         return Response({'message': file_path})
-
-        return Response({'message': '딕셔너리 형태로 응답'})
 
 class ReasonView(APIView):
     def post(self, request, document_id):
-        document = Document.objects.get(id=document_id)
         file_path = get_document_path(document_id)
         
         return Response({'message': file_path})
-
-        return Response({'message': '딕셔너리 형태로 응답'})
 
 class EvaluationView(APIView):
     def post(self, request, document_id):
-        document = Document.objects.get(id=document_id)
         file_path = get_document_path(document_id)
         
         return Response({'message': file_path})
-
-        return Response({'message': '딕셔너리 형태로 응답'})
