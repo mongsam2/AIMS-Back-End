28c7d195c9eee35e890e2684ef5f1654cf3d87cf
yeyechu <yeyechuuu@gmail.com>
2025-01-25 10:49:53 +0000

    [refactor] #30 - solar 반환 자료형 변경

--- aims/utils/ocr_refine.py
+++ aims/utils/ocr_refine.py
@@ -2,13 +2,13 @@
 
 from openai import OpenAI # openai==1.52.2
 
-def refine_ocr_with_solar(api_key, content, prompt, temperature=0.7, ):
+def refine_ocr_with_solar(api_key, content, prompt, temperature=0.7):
     client = OpenAI(
         api_key=api_key,
         base_url="https://api.upstage.ai/v1/solar"
     )
 
-    stream = client.chat.completions.create(
+    response = client.chat.completions.create(
         model="solar-pro",
         messages=[
         {
@@ -20,13 +20,8 @@ def refine_ocr_with_solar(api_key, content, prompt, temperature=0.7, ):
             "content": content
         }
     ],
-        stream=True,
+        stream=False,
         temperature=temperature
     )
-    
-    for chunk in stream:
-        if chunk.choices[0].delta.content is not None:
-            print(chunk.choices[0].delta.content, end="")
-    
-    # Use with stream=False
-    # print(stream.choices[0].message.content)
+
+    return response.choices[0].message.content
