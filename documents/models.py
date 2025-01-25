a153c60545199d4fe08b60dcfaeaa69028d2a579
yeyechu <yeyechuuu@gmail.com>
2025-01-25 13:10:18 +0000

    [refactor] #30 - document 속성 수정

--- documents/models.py
+++ documents/models.py
@@ -20,16 +20,19 @@ class DocumentStateChoices(models.TextChoices):
 
 
 class Document(models.Model):
-    document_type = models.CharField(max_length=50)
+    document_type = models.CharField(max_length=50, choices=DocumentTypeChoices.choices, default=DocumentTypeChoices.알수없음)
     student = models.ForeignKey('students.Student', on_delete=models.CASCADE, related_name='documents')
     upload_date = models.DateTimeField(auto_now_add=True)
     state = models.CharField(max_length=10, choices=DocumentStateChoices.choices, default=DocumentStateChoices.제출)
     criteria = models.ForeignKey('aims.EssayCriteria', on_delete=models.CASCADE, null=True, blank=True)
 
+
     def upload_to(self, filename):
         return f'documents/{self.document_type}/{filename}'
 
+
     file_url = models.FileField(upload_to=upload_to)
+
 
     def __str__(self):
         return f"{self.student}의 {self.document_type}({self.state})"
