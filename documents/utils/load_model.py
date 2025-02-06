18cad8daa6b23c8d9416c5c301c356b6727c8f68
yeyechu <yeyechuuu@gmail.com>
2025-02-06 00:51:48 +0000

    [rename] #81 - 파일 이름 변경 및 삭제

--- documents/utils/load_model.py
+++ documents/utils/load_model.py
@@ -1,15 +1,13 @@
 import os
 import torch
-
+import onnxruntime
 from torchvision import models
 
 from django.conf import settings
-
-
 MODEL_DIR = os.path.join(settings.BASE_DIR, "documents/models")
 
 
-def load_model(model_name="student_model.pth"):
+def load_pytorch_model(model_name="student_model.pth"):
     
     model_path = os.path.join(MODEL_DIR, model_name)
     
@@ -24,6 +22,19 @@ def load_model(model_name="student_model.pth"):
     print(f"model load completed: {model_path}")
 
     return model
+
+
+def load_onnx_model(model_name="student_model.onnx"):
+    
+    model_path = os.path.join(MODEL_DIR, model_name)
+
+    if not os.path.exists(model_path):
+        raise FileNotFoundError(f"ONNX 모델 파일이 존재하지 않습니다: {model_path}")
+
+    session = onnxruntime.InferenceSession(model_path, providers=["CPUExecutionProvider"])
+    print(f"ONNX Model Loaded: {model_path}")
+    
+    return session
 
 
 
