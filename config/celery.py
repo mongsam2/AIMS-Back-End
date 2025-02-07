7c3fdf7faedc761852c8f60215ad603272092e49
yeyechu <yeyechuuu@gmail.com>
2025-02-07 05:55:22 +0000

    [refactor] #86 - celery파일 이동

--- config/celery.py
+++ config/celery.py
@@ -1,12 +1,13 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
+from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 app = Celery('aims')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks(['documents', 'aims'])
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
 
