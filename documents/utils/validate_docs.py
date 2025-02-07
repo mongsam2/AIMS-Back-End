0bb76b728e5c452d15107b5df3d4155c2386a734
yeyechu <yeyechuuu@gmail.com>
2025-02-07 03:41:14 +0000

    [add] #86 - 유사도 계산 함수 반환형태 변경

--- documents/utils/validate_docs.py
+++ documents/utils/validate_docs.py
@@ -32,11 +32,20 @@ def is_doc_type_valid(e_type, d_type):
 
 
 def similarity(queries, text_vectors):
-    similarity_list = []
-    for passage_embedding in text_vectors:
-        similarity = np.dot(passage_embedding, text_vectors)
-        similarity_list.append(similarity)
-        
-    most_similar_result = queries[np.argmax(similarity_list)]
+    """
+    각 query에 대해 text_vectors와의 유사도를 계산하고 유사도 값 리스트를 반환합니다.
 
-    return most_similar_result
+    Args:
+        queries (list): 비교할 query 리스트
+        text_vectors (list): 기준이 되는 벡터 리스트
+
+    Returns:
+        list: 각 query에 대한 유사도 값 리스트
+    """
+    similarity_results = []
+
+    for query_embedding in queries:
+        similarity_list = [np.dot(query_embedding, passage_embedding) for passage_embedding in text_vectors]
+        similarity_results.append(similarity_list)
+
+    return similarity_results
