4a8cd7c8d50ac1ff892e85620b2c847ce90d93fb
yeyechu <yeyechuuu@gmail.com>
2025-02-08 05:33:41 +0000

    [style] #86 - 함수 위치 조정

--- documents/utils/save_vectors_to_compare.py
+++ documents/utils/save_vectors_to_compare.py
@@ -3,36 +3,25 @@ import json
 #from aims.tasks import execute_embedding
 
 from openai import OpenAI
+
+api_key = ""
+json_path = "/data/ephemeral/home/aims_be/documents/vectors/validations2.json"
+
+
 def execute_embedding(queries, api_key):
-
-    """
-    1개 이상 100개 미만의 토큰에 대한 임베딩을 일괄 처리
-    각 텍스트에 대한 토큰은 4,000 미만이어야 하고
-    리스트 전체의 토큰 수는 204,800보다 작아야 한다
-
-    Args:
-        queries(list): 리스트 형태의 text
-        api_key(str): 업스테이지 API 키
-
-    Returns:
-        embedding(list): 리스트 형태의 embedding 결과
-    """
     client = OpenAI(
         api_key=api_key,
         base_url="https://api.upstage.ai/v1/solar"
     )
     
     query_embedding = client.embeddings.create(
-        model = "embedding-query",
+        model = "embedding-passage",
         input = queries
     ).data
 
     embedding_list = [i.embedding for i in query_embedding]
 
     return embedding_list
-
-api_key = ""
-json_path = "/data/ephemeral/home/aims_be/documents/vectors/validations2.json"
 
 """
     1. criteria의 value 값을 추출하여
