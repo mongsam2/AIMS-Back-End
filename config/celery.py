423ad475da846d8c640172476cd38103ca05224a
yeyechu <yeyechuuu@gmail.com>
2025-02-08 04:18:21 +0000

    [refactor] #86 - celery 설정 변경 - celery 파일 이동

--- config/celery.py
+++ config/celery.py
@@ -5,9 +5,9 @@ from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-app = Celery('aims')
+app = Celery('config')
 
-app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+app.config_from_object("django.conf:settings", namespace='CELERY')
+app.autodiscover_tasks()
 
 
